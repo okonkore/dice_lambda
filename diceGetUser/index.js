7c8ba0af3a71c3567b61fbff70dd1c9dc8dbd01b
okonkore <terrarium@MacBook.local>
2019-03-21 23:20:57 +0900

    use utils module

--- diceGetUser/index.js
+++ diceGetUser/index.js
@@ -1,36 +1,25 @@
 "use strict";
 var dao = require('dice-dao');
 var master = require('dice-master');
+var utils = require('dice-utils');
 console.log(master);
 var time = new Date();
-
-var getNextPos = function(current, dice){
-    console.log(master);
-    let next = master.board[current].next;
-    if(dice == 1){
-        return next;
-    }
-    let nextList = [];
-    for(let i = 0; i < next.length; i++){
-        let l = getNextPos(next[i], dice -1);
-        nextList = nextList.concat(l);
-    }
-    return nextList;
-}
 
 exports.handler = async (event) => {
     console.log("diceGetUser");
     console.log(dao);
     let u = event.params.querystring.uuid;
-    
+
     let current = 23;
-    console.log(getNextPos(current, 10));
-    
+    let pos = utils.getNextPos(current, 10);
+    console.log(utils.getNextPos(current, 10));
+
     let data = await dao.getDiceUser(u);
-    
+    let body = data["Item"];
+    body["pos"] = pos;
     const response = {
         statusCode: 200,
-        body: JSON.stringify(data["Item"]),
+        body: JSON.stringify(body),
     };
     return response;
 };
--- diceUpdateDice/index.js
+++ diceUpdateDice/index.js
@@ -1,14 +1,15 @@
 var dao = require('dice-dao');
+var utils = require('dice-utils');
 
 exports.handler = async (event) => {
     let u = event.uuid;
     let dice = Math.floor((Math.random() * 6)) + 1;
-    
+
     await dao.updateDice(u, dice);
-    
+
     const response = {
         statusCode: 200,
-        body: JSON.stringify({"dice" : dice, 'uuid' : event.uuid}),
+        body: JSON.stringify({ "dice": dice, 'uuid': event.uuid }),
     };
     return response;
 };
